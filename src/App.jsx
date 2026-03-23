31622d612ccde06c4fc51bcdf4d8e5e511a4e7f5
Martin Fenton <martin_fenton@hotmail.co.uk>
2026-03-23 11:46:57 +0000

    feat: configured dev proxy to bypass CORS restrictions

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    fetch("/api/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return <h1>Restaurant Finder</h1>;
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // Proxy all requests starting with /api to the Just Eat API
+      "/api": {
+        target: "https://uk.api.just-eat.io",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
