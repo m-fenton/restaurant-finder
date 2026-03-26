a846bb07c68261a5b548821c729c748497031f98
Martin Fenton <martin_fenton@hotmail.co.uk>
2026-03-26 18:05:18 +0000

    feat(components): add RestaurantCard component

--- src/components/RestaurantCard/RestaurantCard.jsx
+++ src/components/RestaurantCard/RestaurantCard.jsx
@@ -1,23 +1,42 @@
 import "./RestaurantCard.css";
 
-function RestaurantCard(props) {
-  const restaurant = props.restaurant;
-
+// Main Restaurant Card component
+// Display split into text and image sections at either side of the card
+function RestaurantCard({ restaurant }) {
   return (
-    <div>
+    <div className="card-wrapper">
+      {/* LEFT SIDE: Text Container */}
+      <div className="card-text-container">
+        <h2>{restaurant.name}</h2>
+        <div className="subject-wrapper">
+          <p className="sub-heading-text">Cuisines</p>
+          <p className="cuisine-text">{restaurant.cuisines}</p>
+        </div>
+        <div className="subject-wrapper">
+          <p className="sub-heading-text">Star Rating</p>
+          <p className="rating-text">
+            ★ {restaurant.rating || restaurant.starRating}
+          </p>
+        </div>
+        <div className="subject-wrapper">
+          <p className="sub-heading-text">Address</p>
+          <p className="address-text">{restaurant.fullAddress}</p>
+        </div>
+      </div>
+
+      {/* RIGHT SIDE: Logo */}
       <img
-        src={restaurant.logoURL || "https://placehold.co/100x100"}
-        alt={restaurant.name}
-        style={{ width: "100px", height: "auto" }}
+        className="card-logo"
+        src={
+          restaurant.logoURL ||
+          "https://images.seeklogo.com/logo-png/40/1/just-eat-logo-png_seeklogo-408326.png"
+        }
+        alt={`${restaurant.name} logo`}
         onError={(e) => {
-          e.target.src = "https://placehold.co/100x100";
+          e.target.src =
+            "https://images.seeklogo.com/logo-png/40/1/just-eat-logo-png_seeklogo-408326.png";
         }}
       />
-      <h2>{restaurant.name}</h2>
-      <p>Cuisines: {restaurant.cuisines}</p>
-      <p>Star Rating: {restaurant.rating}</p>
-      <p>Address: {restaurant.fullAddress}</p>
-      <hr />
     </div>
   );
 }
